0dbc07ee85d601817240e4ae98779e9a31acfff0
손효재 <sonjw5128@gmail.com>
2023-01-13 01:32:33 +0900

    test: CalculatorTest checkNumber test, OperatorTest 구현

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,45 +11,14 @@ class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    @DisplayName("덧셈 테스트")
-    void plus_테스트() {
-        assertThat(calculator.calculate("1 + 2")).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("뺄셈 테스트")
-    void minus_테스트() {
-        assertThat(calculator.calculate("2 - 1")).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("곱셈 테스트")
-    void multiply_테스트() {
-        assertThat(calculator.calculate("2 * 1")).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("나눗셈 테스트")
-    void divide_테스트() {
-        assertThat(calculator.calculate("6 / 3")).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("0으로 나눗셈 테스트")
-    void divide_zero_테스트() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> calculator.calculate("6 / 0"));
-    }
-
-    @Test
     @DisplayName("문자열 계산기 테스트")
     void calculate_테스트() {
         assertThat(calculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
     }
 
     @Test
-    @DisplayName("잘못된 연산자 예외 테스트")
-    void wrong_operator_exception_테스트() {
+    @DisplayName("잘못된 문자열 예외 테스트")
+    void wrong_string_exception_테스트() {
         assertThatThrownBy(() -> {
             calculator.calculate("2 $ 3 + 4");
         }).isInstanceOf(IllegalArgumentException.class)
@@ -56,7 +26,16 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("잘못된 숫자 예외 테스트")
+    @DisplayName("숫자 체크 테스트")
+    void wrong_number_check_테스트() {
+        Assertions.assertAll(
+                () -> assertThat(calculator.checkNumber("2")).isTrue(),
+                () -> assertThat(calculator.checkNumber("+")).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("잘못된 숫자 계산 예외 테스트")
     void wrong_number_exception_테스트() {
         assertThatThrownBy(() -> {
             calculator.calculate("2 + + 3");
