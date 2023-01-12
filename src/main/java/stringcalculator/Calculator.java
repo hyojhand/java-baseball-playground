6a821709708f664f890129fae95c2b838044b0f2
손효재 <sonjw5128@gmail.com>
2023-01-13 01:30:13 +0900

    refactor: 문자열 split List, checkNumber 메서드

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,5 +1,6 @@
 package stringcalculator;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Calculator {
@@ -7,27 +8,32 @@ public class Calculator {
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
 
     public int calculate(String value) {
-        String[] split = value.split(" ");
+        List<String> splitList = List.of(value.split(" "));
 
-        int result = checkNumber(split[0]);
+        String firstValue = splitList.get(0);
+        if(!checkNumber(firstValue)) {
+            throw new IllegalArgumentException("잘못된 숫자 입니다");
+        }
 
-        for (int i = 2; i < split.length; i += 2) {
-            String symbol = split[i - 1];
+        int result = Integer.parseInt(firstValue);
+
+        for (int i = 2; i < splitList.size(); i += 2) {
+            String symbol = splitList.get(i - 1);
             Operator operator = Operator.findSymbol(symbol);
 
-            int number = checkNumber(split[i]);
-            result = operator.calculate(result, number);
+            String secondValue = splitList.get(i);
+            if(!checkNumber(secondValue)) {
+                throw new IllegalArgumentException("잘못된 숫자 입니다");
+            }
+
+            result = operator.calculate(result, Integer.parseInt(secondValue));
         }
 
         return result;
     }
 
-    private int checkNumber(String number) {
-        if (!NUMBER_PATTERN.matcher(number).matches()) {
-            throw new IllegalArgumentException("잘못된 숫자 입니다");
-        }
-
-        return Integer.parseInt(number);
+    boolean checkNumber(String value) {
+        return NUMBER_PATTERN.matcher(value).matches();
     }
 
 }
