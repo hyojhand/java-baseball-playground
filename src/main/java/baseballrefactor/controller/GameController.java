40df7cebe77100327be42cd4d4b539dc446b36ba
손효재 <sonjw5128@gmail.com>
2023-01-23 20:24:22 +0900

    feat: baseball game view, controller

---
+++ src/main/java/baseballrefactor/controller/GameController.java
@@ -0,0 +1,52 @@
+package baseballrefactor.controller;
+
+import baseballrefactor.domain.Balls;
+import baseballrefactor.domain.BaseBallGame;
+import baseballrefactor.domain.GameResult;
+import baseballrefactor.domain.GenerateNumber;
+import baseballrefactor.view.InputView;
+import baseballrefactor.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+
+    private static final InputView INPUT_VIEW = new InputView();
+    private static final OutputView OUTPUT_VIEW = new OutputView();
+    private static List<Integer> numbers;
+
+    public static void start() {
+        List<Integer> randomNumbers = generateRandomNumber();
+        BaseBallGame baseBallGame = new BaseBallGame();
+        GameResult gameResult = new GameResult();
+
+        while (!gameResult.isGameEnd()) {
+            numbers = INPUT_VIEW.getNumberList();
+            gameResult = baseBallGame.play(new Balls(randomNumbers), new Balls(numbers));
+            OUTPUT_VIEW.printResult(gameResult);
+        }
+
+        replayGame(INPUT_VIEW.inputReplayValue());
+    }
+
+    private static void replayGame(String replayValue) {
+        if (replayValue.equals("1")) {
+            start();
+        }
+
+        if (replayValue.equals("2")) {
+            end();
+        }
+    }
+
+    private static List<Integer> generateRandomNumber() {
+        GenerateNumber generateRandom = new GenerateNumber();
+        return generateRandom.getRandomNumbers();
+    }
+
+    private static void end() {
+        System.out.println("게임 종료");
+        System.exit(0);
+    }
+
+}
